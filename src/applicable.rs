eca13666031a2135b8f1bf36a653e395cc0e052c
John Funk <john@underscorefunk.com>
2023-05-16 19:20:41 -0500

    Documentation

--- src/applicable.rs
+++ src/applicable.rs
@@ -1,3 +1,78 @@
+/// # Applicable
+///
+/// One of the goals of this crate is to explore techniques that can add
+/// flexibility and simplicity to a code base. This is important because
+/// role playing game tools require flexibility. It's not uncommon to need
+/// to amend or modify an activity retroactively as well.
+///
+/// At the highest level, the pattern used in this crate involves a few
+/// simple steps.
+///
+/// 1) An action is submitted to the application
+/// 2a) The action is applied to the State resulting in a new updated state.
+/// 2b) The action has a component that throws an error, and the state
+///     is not updated.
+///
+/// The following conventions are used:
+///
+/// - A *command* requires mutable access to state and will return a Result
+///   with an Ok new state or Err with the error message.
+/// - A *query* requires a reference to state, should never mutate state, and
+///   returns a value.
+/// - Process is converted into data by using enums to store *command args* as
+///   enum values.
+///
+/// This module handles the specification (and some implementations) of how
+/// a command is applied to state with the `Applicable` trait.
+///
+/// The following are examples of Applicable in use.
+///
+///
+/// Style 1: Cmd + State = Result
+/// ----------------------------
+/// 1) Command (Enum Variant): impl Applicable on an enum of commands.
+///    The apply_to() implementation will match on `self` (the enum variant)
+///    and will return a matched function call with the variant's
+///    destructured values.
+///
+///    `
+///     // impl
+///     match self {
+///         Cmd::TakeDamage(pub_id, dmg) => Character::Damage(state, pub_id, dmg)
+///         ...
+///     }
+///
+///     // in use
+///     let new_state: CmdResult<State> = Cmd::TakeDamage(10, 62).apply_to(state);
+///     // or
+///     let new_state: CmdResult<State> = Cmd::TakeDamage(10, 62).apply_to_default();
+///    `
+///
+///
+/// 2) Vectors of Commands: The `Applicable` trait is implemented on a Vec of Applicables.
+///
+///    `
+///    // in use
+///    vec![ Cmd::TakeDamage, Cmd::ShoutHealthLevel ].apply_to(state);
+///    // or
+///     vec![ Cmd::TakeDamage, Cmd::ShoutHealthLevel ].apply_to_default();
+///    `
+///    Note that a trait is required if we wish to add additional implementations to
+///    types defined in other crates, like Vec<T: Applicable>
+///
+///
+/// Style 2: State + Cmd = Result
+/// ----------------------------
+/// 3) State command chaining: Use the apply() method on a `State`. CmdResult implements
+///    a single use trait to implement apply() as well. Allowing command of any type to be
+///    chained together.
+///
+///    `
+///      let state = State::default()
+///        .apply( Player::Add(1,"APlayer".to_string()))
+///         .apply( Character::Add(2,"ACharacter".to_string()));
+///     `
+
 use crate::prelude::*;
 
 pub type CmdErr = String;
@@ -10,7 +85,7 @@ pub trait Applicable {
 
 /// Allow a vector of things that can be applied
 /// to be applied directly from the vector.
-impl <T: Applicable> Applicable for Vec<T> {
+impl<T: Applicable> Applicable for Vec<T> {
     fn apply_to(self, state: State) -> CmdResult<State> {
         self.into_iter()
             .fold(
@@ -24,10 +99,9 @@ impl <T: Applicable> Applicable for Vec<T> {
             )
     }
     fn apply_to_default(self) -> CmdResult<State> {
-        self.apply_to( State::default() )
+        self.apply_to(State::default())
     }
 }
-
 
 impl State {
     /// A `State` has the ability to have `Applicable` commands run on it.
@@ -50,17 +124,22 @@ impl State {
     ///
     /// ```
     ///
-    pub fn apply<T: Applicable> (self, command: T) -> CmdResult<State> {
+    pub fn apply<T: Applicable>(self, command: T) -> CmdResult<State> {
         command.apply_to(self)
     }
 }
 
+/// Specify a single use trait so that we can add impl blocks to types
+/// defined outside of this crate.
 pub trait ApplicableChainable {
     fn apply<T: Applicable>(self, command: T) -> CmdResult<State>;
 }
 
+/// Implement our single use trait for orphan implementations
 impl ApplicableChainable for CmdResult<State> {
-    fn apply <T: Applicable>(self, command: T) -> CmdResult<State> {
+    /// Used to chain results from applying commands.
+    /// See impl State { apply() } for an example.
+    fn apply<T: Applicable>(self, command: T) -> CmdResult<State> {
         match self {
             Ok(state) => state.apply(command),
             Err(_) => self
