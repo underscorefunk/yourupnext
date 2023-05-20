7a3cab9c809dc259cd4f0e35a3cff3df7db6cfc0
John Funk <john@underscorefunk.com>
2023-05-20 16:03:25 -0500

    Add remove turn fn

--- src/component/turn_order.rs
+++ src/component/turn_order.rs
@@ -42,7 +42,47 @@ pub mod cmd {
         Ok(state)
     }
 
-    // COMMAND > Remove turn
+    /// COMMAND > Remove turn
+    /// ```
+    /// use yourupnext::prelude::*;
+    ///
+    /// let scenario_pub_id = 100;
+    /// let state = State::default()
+    ///         .apply( Scenario::Add(scenario_pub_id) )
+    ///         .apply( Character::Add(200, "ACharacter") )
+    ///         .apply( Character::Add(300, "BCharacter") )
+    ///         .apply( |state| turn_order::cmd::add_turn(state, scenario_pub_id, 200))
+    ///         .apply( |state| turn_order::cmd::add_turn(state, scenario_pub_id, 300))
+    ///         .apply( |state| turn_order::cmd::remove_turn(state, scenario_pub_id, 200))
+    ///         .unwrap();
+    ///
+    /// assert_eq!(turn_order::qry::sequence(&state, scenario_pub_id), vec![300] );
+    /// ```
+    pub fn remove_turn(mut state: State, scenario_pub_id: PubId, turn_entity_pub_id: PubId ) -> CmdResult<State>{
+
+        if !scenario::qry::exists(&state, scenario_pub_id) {
+            return Err("Can not remove turn for nonexistant or nonscenario entity".to_string());
+        }
+
+        if !entity::qry::exists(&state, turn_entity_pub_id) {
+            return Err("Can not remove nonexitant entity to a scenario's turn order".to_string());
+        }
+
+        let scenario_id = scenario::qry::id(&state, scenario_pub_id);
+        let mut sequence = state.turn_order.get(scenario_id).unwrap_or_default();
+        let entity_id = entity::qry::id(&state, turn_entity_pub_id);
+
+        if ! sequence.contains(&entity_id) {
+            return Err("Can not remove turn that isn't in the turn order".to_string() );
+        }
+
+        sequence.retain(|sequenced_id| sequenced_id != &entity_id );
+        state.turn_order.update( scenario_id, sequence );
+
+        Ok(state)
+
+    }
+
 
     // COMMAND > Reorder turn
 
